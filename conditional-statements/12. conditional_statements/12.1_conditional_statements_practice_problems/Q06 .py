46726963a74f6e5e8cb29fcfc9e35f5992a68e99
Hari46-om <hariom.yadav.cg@gmail.com>
2026-09-26 20:22:53 +0530

    added 6th question

---
+++ conditional-statements/12. conditional_statements/12.1_conditional_statements_practice_problems/Q06 .py
@@ -0,0 +1,17 @@
+# -----------------------------------------------------------------------------------------------
+# =========================================Question 6: ==========================================
+# -----------------------------------------------------------------------------------------------
+
+# 6.
+# Write a program that checks whether a number is positive or not.
+
+# Expected messages:
+
+# Positive
+# Not positive
+
+num=int(input("Enter a number: "))
+if num>0:
+    print("Positive")
+else:
+    print("Not positive")
